da9fe3386456f13b2147e69b85d57b87a32e5702
KoneNeil <koneneil@gmail.com>
2026-02-17 14:45:07 +0100

    add service + repository

--- src/main/java/com/repositories/MovieRepository.java
+++ src/main/java/com/repositories/MovieRepository.java
@@ -1,4 +1,22 @@
 package com.repositories;
 
-public class MovieRepository {
+import com.entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+
+    // Trouver les films par genre
+    List<Movie> findByGenresContaining(String genre);
+
+    // Trouver les films d'un réalisateur précis
+    List<Movie> findByRealisateur(String realisateur);
+
+    // Trouver les films où un acteur précis joue
+    List<Movie> findByActeursContaining(String acteur);
+
+    // Trouver un film par son titre unique
+    Movie findByTitle(String title);
 }
--- src/main/java/com/services/MovieService.java
+++ src/main/java/com/services/MovieService.java
@@ -1,4 +1,32 @@
 package com.services;
 
-public interface MovieService {
+import com.entities.Movie;
+import com.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Movie createMovie(Movie movie) {
+        movie.setEstOuvertALaLocation(false);
+        return movieRepository.save(movie);
+    }
+
+    public void toggleLocationStatus(int movieId, boolean status) {
+        Movie movie = movieRepository.findById(movieId)
+                .orElseThrow(() -> new RuntimeException("Film non trouvé"));
+        movie.setEstOuvertALaLocation(status);
+        movieRepository.save(movie);
+    }
+
 }
