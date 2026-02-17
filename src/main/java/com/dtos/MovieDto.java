716c1d777bff4a6229ed9f6cec098067468141b0
kerzil-hugo <hugokerzil@gmail.com>
2026-02-17 14:52:50 +0100

    Modification du fichier MovieDto.java et du fichier MovieMapper.java

--- src/main/java/com/dtos/MovieDto.java
+++ src/main/java/com/dtos/MovieDto.java
@@ -29,4 +29,84 @@ public class MovieDto {
     private String affiche;
 
     private boolean estOuvertALaLocation;
+
+    // --- Constructeurs ---
+
+    public MovieDto() {
+    }
+
+    // --- Getters et Setters ---
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAnneeRealisation() {
+        return anneeRealisation;
+    }
+
+    public void setAnneeRealisation(String anneeRealisation) {
+        this.anneeRealisation = anneeRealisation;
+    }
+
+    public String getRealisateur() {
+        return realisateur;
+    }
+
+    public void setRealisateur(String realisateur) {
+        this.realisateur = realisateur;
+    }
+
+    public List<String> getActeurs() {
+        return acteurs;
+    }
+
+    public void setActeurs(List<String> acteurs) {
+        this.acteurs = acteurs;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
+    public int getAgeMinimum() {
+        return ageMinimum;
+    }
+
+    public void setAgeMinimum(int ageMinimum) {
+        this.ageMinimum = ageMinimum;
+    }
+
+    public String getAffiche() {
+        return affiche;
+    }
+
+    public void setAffiche(String affiche) {
+        this.affiche = affiche;
+    }
+
+    // Pour un booléen primitif, la convention est "is" au lieu de "get"
+    public boolean isEstOuvertALaLocation() {
+        return estOuvertALaLocation;
+    }
+
+    public void setEstOuvertALaLocation(boolean estOuvertALaLocation) {
+        this.estOuvertALaLocation = estOuvertALaLocation;
+    }
 }
--- src/main/java/com/mappers/MovieMapper.java
+++ src/main/java/com/mappers/MovieMapper.java
@@ -1,4 +1,46 @@
 package com.mappers;
 
+import com.dtos.MovieDto;
+import com.entities.Movie;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MovieMapper {
+
+    public MovieDto toDto(Movie movie) {
+        if (movie == null) {
+            return null;
+        }
+        MovieDto dto = new MovieDto();
+        dto.setId(movie.getId());
+        dto.setTitle(movie.getTitle());
+        dto.setAnneeRealisation(movie.getAnneeRealisation());
+        dto.setRealisateur(movie.getRealisateur());
+        dto.setActeurs(movie.getActeurs());
+        dto.setGenres(movie.getGenres());
+        dto.setAgeMinimum(movie.getAgeMinimum());
+        dto.setAffiche(movie.getAffiche());
+        dto.setEstOuvertALaLocation(movie.isEstOuvertALaLocation());
+        return dto;
+    }
+
+    public Movie toEntity(MovieDto dto) {
+        if (dto == null) {
+            return null;
+        }
+        Movie movie = new Movie();
+        if (dto.getId() != null) {
+            movie.setId(dto.getId());
+        }
+        movie.setTitle(dto.getTitle());
+        movie.setAnneeRealisation(dto.getAnneeRealisation());
+        movie.setRealisateur(dto.getRealisateur());
+        movie.setActeurs(dto.getActeurs());
+        movie.setGenres(dto.getGenres());
+        movie.setAgeMinimum(dto.getAgeMinimum());
+        movie.setAffiche(dto.getAffiche());
+        movie.setEstOuvertALaLocation(dto.isEstOuvertALaLocation());
+        return movie;
+    }
+
 }
