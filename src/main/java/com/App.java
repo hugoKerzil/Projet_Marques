0c2ca62f0b40064bae5e7218fe4546b67b474f54
KoneNeil <koneneil@gmail.com>
2026-02-17 11:45:40 +0100

    setup

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,7 +1,10 @@
 package com;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class App {
@@ -10,4 +13,13 @@ public class App {
 		SpringApplication.run(App.class, args);
 	}
 
+	@Bean
+	CommandLineRunner rapide(JdbcTemplate jdbc) {
+		return args -> {
+			// On demande juste au serveur de nous donner son nom ou l'heure
+			String result = jdbc.queryForObject("SELECT DATABASE()", String.class);
+			System.out.println("CONNEXION RÉUSSIE !");
+			System.out.println("Tu es actuellement connecté à la base : " + result);
+		};
+	}
 }
